b14968be87bcfe0ff442dbd28bc202e3756d87e7
MuhammadZikri <zikrim577@gmail.com>
2022-10-05 12:44:29 +0700

    Create React App

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./component/Navbar";
+
+export default function App() {
+    return (
+        <div>
+            <Navbar />
+            <div>
+                <h1>Basic React JS - 1 </h1>
+            </div>
+        </div>
+    )
+}
---
+++ src/component/Navbar.jsx
@@ -0,0 +1,8 @@
+
+export default function Navbar() {
+    return (
+        <nav className="navbar">
+            <h1>Muhammad Zikri</h1>
+        </nav>
+    )
+}
